f5e093b2ae612871ef671a97eba5e9aff3d92498
Ross Wang <rosswang@RossdeMacBook-Pro.local>
2026-07-18 11:35:39 +0800

    部署合并 PR #1 的蓝皮书更新

---
+++ assets/chunks/channel.tnPq8Gw1.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./mermaid.core.Bhdy4Fw5.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- assets/chunks/swimlanesDiagram-G3AALYLV.CJ_tt-dF.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.ByroFZgV.js";import{_ as a}from"./mermaid.core.IgWhUqKk.js";import"./chunk-5VM5RSS4.BHQi_f0F.js";import"./chunk-XXDRQBXY.C-An6F2l.js";import"./chunk-VR4S4FIN.Cb9glGTH.js";import"./chunk-32BRIVSS.nP7r3VHa.js";import"./channel.BrnB2zoM.js";import"./framework.BnBoz4Zl.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
